f7acad385bfe548320f6a1d7f722cbfbb55bd429
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-11-16 20:09:49 -0300

    Add @nestjs/mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    JogadoresModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:qsj7O89qPeC3PM3Z@api-smartranking.kqk4x.mongodb.net/smartranking?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
